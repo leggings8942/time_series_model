7d3626dde6b8e1af96afac2e55cbc966e8d16d86
h_nagino <h_nagino@ADN23078MacBook-Pro.local>
2024-10-16 13:56:18 +0900

    DF検定の実装完了

--- test.py
+++ test.py
@@ -10,114 +10,17 @@ import pyspark as ps
 from statsmodels.tsa.vector_ar.var_model import VAR
 from statsmodels.tsa.stattools import adfuller
 
-ps_conf = ps.SparkConf().set("spark.logConf", "false")\
-            .set("spark.executor.memory", "12g")\
-            .set("spark.driver.memory", "4g")\
-            .set("spark.executor.cores", "7")\
-            .set("spark.sql.shuffle.partitions", "500")\
-            .set("spark.executor.extraJavaOptions", "-XX:+UseG1GC -XX:+UseStringDeduplication")\
-            .set("spark.eventLog.gcMetrics.youngGenerationGarbageCollectors", "G1 Young Generation")\
-            .set("spark.eventLog.gcMetrics.oldGenerationGarbageCollectors", "G1 Old Generation")\
-			.set("spark.logConf", "false")
-spark = SparkSession.builder.config(conf=ps_conf).getOrCreate()
-
 SPECIFIED_PATH = "csv_data/"
 SPECIFIED_DATE = "20240918"
 SPECIFIED_CSV  = SPECIFIED_PATH + SPECIFIED_DATE
 
-input_path = SPECIFIED_CSV + "_c-united_config.csv"
-df_config  = spark.read.option("inferSchema", "True").option("header", "True").csv(input_path)
-df_config.persist(StorageLevel.MEMORY_AND_DISK_DESER)
+pd_data = pd.read_csv(SPECIFIED_CSV + "_urp_data.csv")
+result = adfuller(pd_data.to_numpy(), maxlag=0, autolag=None, regression="ct")
 
-utid_list = df_config.select("unit_id").drop_duplicates().rdd.flatMap(lambda x: x).collect()
-spid_list = df_config.select("shop_id").drop_duplicates().rdd.flatMap(lambda x: x).collect()
-
-#pos data 前処理
-input_path  = SPECIFIED_CSV + "_pos_data_table.csv"
-df_pos_data = spark.read.option('inferSchema', 'True').option('header', 'True').csv(input_path)\
-				.select(
-					"shop_id",
-                    "レシートＮｏ．",
-                    "商品種別",
-                    "商品コード",
-                    F.regexp_replace(col("商品名称（または券名称）"), "[ 　]", "").alias("商品名称（または券名称）"),
-                    "オーダー時刻",
-                    "単価",
-                    "数量",
-                    "合計金額",
-                    "date"
-				)\
-				.filter(col("商品名称（または券名称）") != "")\
-				.groupBy("shop_id", "date", "レシートＮｏ．").agg(
-                    F.last("オーダー時刻").alias("オーダー時刻"),
-                    F.sum(F.when(col("商品種別") == "Y", 1).otherwise(0)).alias("レシートあたりのセット商品の数"),
-                    F.sum("数量").alias("総売上点数"),
-                    F.sum("合計金額").alias("総売上"),
-				)\
-                .withColumn("レシートあたりのセット商品の数", F.when(col("レシートあたりのセット商品の数") == 0, 1)
-                            								.otherwise(col("レシートあたりのセット商品の数")))\
-                .withColumn("オーダー時刻", (F.col("オーダー時刻") / 100).cast("int"))\
-                .withColumnRenamed("レシートあたりのセット商品の数", "来店者数")\
-                .withColumnRenamed("オーダー時刻", "hour")
-df_pos_data = df_pos_data.groupBy("shop_id", "date", "hour").agg(
-                    F.sum("来店者数").alias("来店者数"),
-                    F.sum("総売上点数").alias("総売上点数"),
-                    F.sum("総売上").alias("総売上"),
-				)\
-                .select(["shop_id", "date", "hour", "来店者数", "総売上点数", "総売上"])\
-                .orderBy(col("shop_id").asc(), col("date").asc(), col("hour").asc())
-
-df_pos_data = df_pos_data\
-    				.withColumn("date", F.from_unixtime(F.unix_timestamp("date") + F.col("hour") * 3600))\
-                    .drop("hour")\
-                    .orderBy(col("shop_id").asc(), col("date").asc())
-
-df_pos_data = df_pos_data\
-					.join(df_config.select(["shop_id", "caption"]), on="shop_id", how="inner")\
-                    .select(["shop_id", "caption", "date", "来店者数", "総売上点数", "総売上"])\
-                    .orderBy(col("shop_id").asc(), col("date").asc())
-
-pd_pos_data  = df_pos_data.select(["shop_id", "caption", "date", "来店者数"]).toPandas()
-pd_tmp_data1 = pd_pos_data[pd_pos_data["shop_id"] == 1189] # カフェ・ド・クリエグランサンシャイン通り店
-pd_tmp_data2 = pd_pos_data[pd_pos_data["shop_id"] == 1616] # カフェ・ド・クリエ日比谷通り内幸町店
-pd_tmp_data3 = pd_pos_data[pd_pos_data["shop_id"] == 1428] # カフェ・ド・クリエ札幌オーロラタウン店
-pd_tmp_data4 = pd_pos_data[pd_pos_data["shop_id"] == 1550] # カフェ・ド・クリエ博多大博通店
-pd_pos_data  = pd.merge(pd_tmp_data1, pd_tmp_data2, on="date", how="inner", suffixes=['_1', '_2'])
-pd_pos_data  = pd.merge(pd_pos_data,  pd_tmp_data3, on="date", how="inner", suffixes=['_2', '_3'])
-pd_pos_data  = pd.merge(pd_pos_data,  pd_tmp_data4, on="date", how="inner", suffixes=['_3', '_4'])
-pd_pos_data  = pd_pos_data[["date", "来店者数_1", "来店者数_2", "来店者数_3", "来店者数_4"]]
-pd_pos_data  = pd_pos_data.rename(columns={
-    								"来店者数_1": "カフェ・ド・クリエグランサンシャイン通り店",
-                                    "来店者数_2": "カフェ・ド・クリエ日比谷通り内幸町店",
-                                    "来店者数_3": "カフェ・ド・クリエ札幌オーロラタウン店",
-                                    "来店者数_4": "カフェ・ド・クリエ博多大博通店"
-                                })
-pd_pos_data  = pd_pos_data[[
-    				"date",
-                    "カフェ・ド・クリエグランサンシャイン通り店",
-                    "カフェ・ド・クリエ日比谷通り内幸町店",
-                    "カフェ・ド・クリエ札幌オーロラタウン店",
-                    "カフェ・ド・クリエ博多大博通店"
-                ]]
-del pd_tmp_data1
-del pd_tmp_data2
-del pd_tmp_data3
-del pd_tmp_data4
-
-x_data = pd_pos_data[["カフェ・ド・クリエグランサンシャイン通り店", "カフェ・ド・クリエ日比谷通り内幸町店", "カフェ・ド・クリエ札幌オーロラタウン店", "カフェ・ド・クリエ博多大博通店"]].values.tolist()
-x_train, x_test = x_data[0:3600], x_data[3600:]
-
-# VARモデルの作成とフィット
-var_model = VAR(x_test)
-
-var_fit = var_model.fit(4)
-var_model.select_order(maxlags=20)
-IRF  = var_fit.irf(1)
-FEVD = var_fit.fevd(30)
-
-s = var_fit.sigma_u
-p = np.linalg.cholesky(var_fit.sigma_u)
-print(IRF.orth_irfs.round(4))
-print((IRF.orth_irfs / np.diag(p)).round(4))
-#result = adfuller(pd_pos_data["カフェ・ド・クリエグランサンシャイン通り店"].to_list(), autolag="BIC", regression="ct")
-#print(result)
+print("ADF Statistic: %f" % result[0])
+print("p-value: %f" % result[1])
+print("used lag: %d" % result[2])
+print("data num: %d" % result[3])
+for key, value in result[4].items():
+    print('\t%s: %.3f' % (key, value))
+print("ic best: %f" % result[5])
--- time_series_model.py
+++ time_series_model.py
@@ -3,7 +3,6 @@ import scipy as sp
 import pandas as pd
 from scipy import stats
 from random import Random
-from scipy.ndimage import shift
 
 def normal_distribution(x, loc=0, scale=1):
     return stats.norm.pdf(x, loc=loc, scale=scale)
@@ -553,9 +552,10 @@ class Dickey_Fuller_Test:
         if type(test_data) is list:
             test_data = np.array(test_data)
         
-        if test_data.ndim != 2:
-            print(f"test_data dims = {test_data.ndim}")
-            print("エラー：：次元数が一致しません。")
+        if (test_data.ndim != 2) or (test_data.shape[1] != 1):
+            print(f"test_data dims  = {test_data.ndim}")
+            print(f"test_data shape = {test_data.shape}")
+            print("エラー：：次元数または形が一致しません。")
             raise
         
         self.test_data           = test_data
@@ -570,6 +570,8 @@ class Dickey_Fuller_Test:
         self.unbiased_dispersion = 0
         self.dispersion          = 0
         self.ma_inf              = np.zeros([1, 1])
+        self.tvalue              = 0
+        self.pvalue              = 0
     
     def fit(self) -> bool:
         tmp_train_data = self.test_data
@@ -605,11 +607,11 @@ class Dickey_Fuller_Test:
             self.trend_1st, self.trend_2nd = x[s+1, :], np.zeros([1, x.shape[1]])
             
         elif self.regression == "ctt":# 定数項あり&2次のトレンドあり
-            A = np.hstack([x_data, np.ones([num, 1]), np.arange(num).reshape([num, 1]) ** 2])
+            A = np.hstack([x_data, np.ones([num, 1]), np.arange(num).reshape([num, 1]), np.arange(num).reshape([num, 1]) ** 2])
             b = y_data
             x = np.dot(np.linalg.inv(np.dot(A.T, A)), np.dot(A.T, b))
             self.alpha,     self.alpha0    = x[0:s, :], x[s, :]
-            self.trend_1st, self.trend_2nd = np.zeros([1, x.shape[1]]), x[s+1, :]
+            self.trend_1st, self.trend_2nd = x[s+1, :], x[s+2, :]
             
         else:
             raise
@@ -637,7 +639,7 @@ class Dickey_Fuller_Test:
 
         return True
     
-    def predict(self, test_data, time_data) -> np.ndarray:
+    def predict(self, test_data, time_data = np.array([[]])) -> np.ndarray:
         if type(test_data) is pd.core.frame.DataFrame:
             test_data = test_data.to_numpy()
         
@@ -650,10 +652,12 @@ class Dickey_Fuller_Test:
         if type(time_data) is list:
             time_data = np.array(time_data)
         
-        if (test_data.ndim != 2) or (time_data.ndim != 2):
-            print(f"test_data dims = {test_data.ndim}")
-            print(f"time_data dims = {time_data.ndim}")
-            print("エラー：：次元数が一致しません。")
+        if (test_data.ndim != 2) or (test_data.shape[1] != 1) or (time_data.ndim != 2) or (time_data.shape[1] != 1):
+            print(f"test_data dims  = {test_data.ndim}")
+            print(f"test_data shape = {test_data.shape}")
+            print(f"time_data dims  = {time_data.ndim}")
+            print(f"time_data shape = {time_data.shape}")
+            print("エラー：：次元数または形が一致しません。")
             raise
         
         y_pred = np.dot(test_data, self.alpha) + self.alpha0
@@ -668,7 +672,178 @@ class Dickey_Fuller_Test:
             y_pred = np.dot(test_data, self.alpha) + self.alpha0 + self.trend_1st * time_data
             
         elif self.regression == "ctt":# 定数項あり&2次のトレンドあり
-            y_pred = np.dot(test_data, self.alpha) + self.alpha0 + self.trend_2nd * (time_data ** 2)
+            y_pred = np.dot(test_data, self.alpha) + self.alpha0 + self.trend_1st * time_data + self.trend_2nd * (time_data ** 2)
         
         return y_pred
+    
+    def dfRuller(self):
+        tau_star_nc  = [-1.04, -1.53, -2.68, -3.09, -3.07, -3.77]
+        tau_min_nc   = [-19.04, -19.62, -21.21, -23.25, -21.63, -25.74]
+        tau_max_nc   = [np.inf, 1.51, 0.86, 0.88, 1.05, 1.24]
+        tau_star_c   = [-1.61, -2.62, -3.13, -3.47, -3.78, -3.93]
+        tau_min_c    = [-18.83, -18.86, -23.48, -28.07, -25.96, -23.27]
+        tau_max_c    = [2.74, 0.92, 0.55, 0.61, 0.79, 1]
+        tau_star_ct  = [-2.89, -3.19, -3.50, -3.65, -3.80, -4.36]
+        tau_min_ct   = [-16.18, -21.15, -25.37, -26.63, -26.53, -26.18]
+        tau_max_ct   = [0.7, 0.63, 0.71, 0.93, 1.19, 1.42]
+        tau_star_ctt = [-3.21, -3.51, -3.81, -3.83, -4.12, -4.63]
+        tau_min_ctt  = [-17.17, -21.1, -24.33, -24.03, -24.33, -28.22]
+        tau_max_ctt  = [0.54, 0.79, 1.08, 1.43, 3.49, 1.92]
+
+        _tau_maxs = {
+            "n":   tau_max_nc,
+            "c":   tau_max_c,
+            "ct":  tau_max_ct,
+            "ctt": tau_max_ctt,
+        }
+        _tau_mins = {
+            "n":   tau_min_nc,
+            "c":   tau_min_c,
+            "ct":  tau_min_ct,
+            "ctt": tau_min_ctt,
+        }
+        _tau_stars = {
+            "n":   tau_star_nc,
+            "c":   tau_star_c,
+            "ct":  tau_star_ct,
+            "ctt": tau_star_ctt,
+        }
+        
+        
+        small_scaling = np.array([1, 1, 1e-2])
+        tau_nc_smallp = [
+            [0.6344, 1.2378, 3.2496],
+            [1.9129, 1.3857, 3.5322],
+            [2.7648, 1.4502, 3.4186],
+            [3.4336, 1.4835, 3.19],
+            [4.0999, 1.5533, 3.59],
+            [4.5388, 1.5344, 2.9807]]
+        tau_nc_smallp = np.asarray(tau_nc_smallp)*small_scaling
+
+        tau_c_smallp = [
+            [2.1659, 1.4412, 3.8269],
+            [2.92, 1.5012, 3.9796],
+            [3.4699, 1.4856, 3.164],
+            [3.9673, 1.4777, 2.6315],
+            [4.5509, 1.5338, 2.9545],
+            [5.1399, 1.6036, 3.4445]]
+        tau_c_smallp = np.asarray(tau_c_smallp)*small_scaling
+
+        tau_ct_smallp = [
+            [3.2512, 1.6047, 4.9588],
+            [3.6646, 1.5419, 3.6448],
+            [4.0983, 1.5173, 2.9898],
+            [4.5844, 1.5338, 2.8796],
+            [5.0722, 1.5634, 2.9472],
+            [5.53, 1.5914, 3.0392]]
+        tau_ct_smallp = np.asarray(tau_ct_smallp)*small_scaling
+
+        tau_ctt_smallp = [
+            [4.0003, 1.658, 4.8288],
+            [4.3534, 1.6016, 3.7947],
+            [4.7343, 1.5768, 3.2396],
+            [5.214, 1.6077, 3.3449],
+            [5.6481, 1.6274, 3.3455],
+            [5.9296, 1.5929, 2.8223]]
+        tau_ctt_smallp = np.asarray(tau_ctt_smallp)*small_scaling
+
+        _tau_smallps = {
+            "n":   tau_nc_smallp,
+            "c":   tau_c_smallp,
+            "ct":  tau_ct_smallp,
+            "ctt": tau_ctt_smallp,
+        }
+
+
+        large_scaling = np.array([1, 1e-1, 1e-1, 1e-2])
+        tau_nc_largep = [
+            [0.4797, 9.3557, -0.6999, 3.3066],
+            [1.5578, 8.558, -2.083, -3.3549],
+            [2.2268, 6.8093, -3.2362, -5.4448],
+            [2.7654, 6.4502, -3.0811, -4.4946],
+            [3.2684, 6.8051, -2.6778, -3.4972],
+            [3.7268, 7.167, -2.3648, -2.8288]]
+        tau_nc_largep = np.asarray(tau_nc_largep)*large_scaling
+
+        tau_c_largep = [
+            [1.7339, 9.3202, -1.2745, -1.0368],
+            [2.1945, 6.4695, -2.9198, -4.2377],
+            [2.5893, 4.5168, -3.6529, -5.0074],
+            [3.0387, 4.5452, -3.3666, -4.1921],
+            [3.5049, 5.2098, -2.9158, -3.3468],
+            [3.9489, 5.8933, -2.5359, -2.721]]
+        tau_c_largep = np.asarray(tau_c_largep)*large_scaling
+
+        tau_ct_largep = [
+            [2.5261, 6.1654, -3.7956, -6.0285],
+            [2.85, 5.272, -3.6622, -5.1695],
+            [3.221, 5.255, -3.2685, -4.1501],
+            [3.652, 5.9758, -2.7483, -3.2081],
+            [4.0712, 6.6428, -2.3464, -2.546],
+            [4.4735, 7.1757, -2.0681, -2.1196]]
+        tau_ct_largep = np.asarray(tau_ct_largep)*large_scaling
+
+        tau_ctt_largep = [
+            [3.0778, 4.9529, -4.1477, -5.9359],
+            [3.4713, 5.967, -3.2507, -4.2286],
+            [3.8637, 6.7852, -2.6286, -3.1381],
+            [4.2736, 7.6199, -2.1534, -2.4026],
+            [4.6679, 8.2618, -1.822, -1.9147],
+            [5.0009, 8.3735, -1.6994, -1.6928]]
+        tau_ctt_largep = np.asarray(tau_ctt_largep)*large_scaling
+
+        _tau_largeps = {
+            "n":   tau_nc_largep,
+            "c":   tau_c_largep,
+            "ct":  tau_ct_largep,
+            "ctt": tau_ctt_largep,
+        }
+        
+        
+        maxstat  = _tau_maxs[self.regression]
+        minstat  = _tau_mins[self.regression]
+        starstat = _tau_stars[self.regression]
+        
+        
+        nobs      = len(self.test_data)
+        x_data    = np.array([self.test_data[t-self.lags : t][::-1].ravel() for t in range(self.lags, nobs)])
+        num, _    = x_data.shape
+        esti_coef = np.hstack([self.alpha, self.alpha0]).ravel()
+        x_d_all   = np.hstack([x_data, np.ones([num, 1])])
+        if   self.regression == "n":  # 定数項なし&トレンドなし
+            esti_coef = np.hstack([self.alpha]).ravel()
+            x_d_all   = np.hstack([x_data])
+            
+        elif self.regression == "c":  # 定数項あり&トレンドなし
+            esti_coef = np.hstack([self.alpha, self.alpha0]).ravel()
+            x_d_all   = np.hstack([x_data, np.ones([num, 1])])
+        
+        elif self.regression == "ct": # 定数項あり&1次のトレンドあり
+            esti_coef = np.hstack([self.alpha, self.alpha0, self.trend_1st]).ravel()
+            x_d_all   = np.hstack([x_data, np.ones([num, 1]), np.arange(num).reshape([num, 1])])
+            
+        elif self.regression == "ctt":# 定数項あり&2次のトレンドあり
+            esti_coef = np.hstack([self.alpha, self.alpha0, self.trend_1st, self.trend_2nd]).ravel()
+            x_d_all   = np.hstack([x_data, np.ones([num, 1]), np.arange(num).reshape([num, 1]), np.arange(num).reshape([num, 1]) ** 2])
+        
+        
+        tvalue      = (esti_coef - 1) / np.sqrt(np.diag(self.sigma * np.linalg.inv(np.dot(x_d_all.T, x_d_all))))
+        self.tvalue = tvalue[0]
+        if self.tvalue > maxstat[0]:
+            self.pvalue = 1.0
+            return self.pvalue
+        
+        elif self.tvalue < minstat[0]:
+            self.pvalue = 0.0
+            return self.pvalue
+        
+        if self.tvalue <= starstat[0]:
+            tau_coef    = _tau_smallps[self.regression][0]
+            self.pvalue = sp.stats.norm.cdf(np.polyval(tau_coef[::-1], self.tvalue))
+        else:
+            # Note: above is only for z stats
+            tau_coef    = _tau_largeps[self.regression][0]
+            self.pvalue = sp.stats.norm.cdf(np.polyval(tau_coef[::-1], self.tvalue))
+        
+        return self.tvalue, self.pvalue
 
