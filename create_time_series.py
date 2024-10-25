79e34b78509f442d1b2016f5fbd1a6700f6b818e
h_nagino <h_nagino@ADN23078MacBook-Pro.local>
2024-10-25 10:09:25 +0900

    ADF検定の実装完了

--- create_time_series.py
+++ create_time_series.py
@@ -21,18 +21,78 @@ def create_time_data_n(length=1000, scale=1):
     x = np.random.normal(loc=0, scale=scale, size=[length])
     return x.cumsum()
 
+def create_time_data_n_lag(lags=2, length=1000, coef=np.array([0.5, 0.5]), scale=1):
+    x = np.empty(length)
+    x[0] = 0
+    for idx in range(1, lags):
+        tmp = [coef[-idx2] * x[idx - 1 - idx2] for idx2 in range(0, idx)]
+        x[idx] = np.sum(tmp) + np.random.normal(loc=0, scale=scale)
+    
+    for idx in range(lags, length):
+        x[idx] = np.sum(coef * x[idx-lags:idx]) + np.random.normal(loc=0, scale=scale)
+    
+    if np.any(np.isnan(x)) or np.any(np.isinf(x)):
+        x = create_time_data_n_lag(lags, length, scale) 
+    
+    return x
+
 def create_time_data_c(length=1000, scale=1, const=10):
     x = np.random.normal(loc=0, scale=scale, size=[length])
     return const + x.cumsum()
+
+def create_time_data_c_lag(lags=2, length=1000, coef=np.array([0.5, 0.5]), scale=1, const=10):
+    x = np.empty(length)
+    x[0] = const
+    for idx in range(1, lags):
+        tmp = [coef[-idx2] * x[idx - 1 - idx2] for idx2 in range(0, idx)]
+        x[idx] = np.sum(tmp) + np.random.normal(loc=0, scale=scale)
+    
+    for idx in range(lags, length):
+        x[idx] = np.sum(coef * x[idx-lags:idx]) + np.random.normal(loc=0, scale=scale)
+    
+    if np.any(np.isnan(x)) or np.any(np.isinf(x)):
+        x = create_time_data_c_lag(lags, length, scale, const)
+    
+    return x
 
 def create_time_data_ct(length=1000, scale=1, const=10, δ1=2):
     x = δ1 + np.random.normal(loc=0, scale=scale, size=[length])
     return const + x.cumsum()
 
+def create_time_data_ct_lag(lags=2, length=1000, coef=np.array([0.5, 0.5]), scale=1, const=10, δ1=2):
+    x = np.empty(length)
+    x[0] = const
+    for idx in range(1, lags):
+        tmp = [coef[-idx2] * x[idx - 1 - idx2] for idx2 in range(0, idx)]
+        x[idx] = np.sum(tmp) + δ1 + np.random.normal(loc=0, scale=scale)
+    
+    for idx in range(lags, length):
+        x[idx] = δ1 + np.sum(coef * x[idx-lags:idx]) + np.random.normal(loc=0, scale=scale)
+    
+    if np.any(np.isnan(x)) or np.any(np.isinf(x)):
+        x = create_time_data_ct_lag(lags, length, scale, const, δ1)
+    
+    return x
+
 def create_time_data_ctt(length=1000, scale=1, const=10, δ1=2, δ2=1.1):
     t = np.arange(1, length + 1)
     x = δ1 + δ2 * t + np.random.normal(loc=0, scale=scale, size=[length])
     return const + x.cumsum()
+
+def create_time_data_ctt_lag(lags=2, length=1000, coef=np.array([0.5, 0.5]), scale=1, const=10, δ1=2, δ2=1.1):
+    x = np.empty(length)
+    x[0] = const
+    for idx in range(1, lags):
+        tmp = [coef[-idx2] * x[idx - 1 - idx2] for idx2 in range(0, idx)]
+        x[idx] = np.sum(tmp) + δ1 + (idx + 1) * δ2 + np.random.normal(loc=0, scale=scale)
+    
+    for idx in range(lags, length):
+        x[idx] = δ1 + (idx + 1) * δ2 + np.sum(coef * x[idx-lags:idx]) + np.random.normal(loc=0, scale=scale)
+    
+    if np.any(np.isnan(x)) or np.any(np.isinf(x)):
+        x = create_time_data_ctt_lag(lags, length, scale, const, δ1, δ2)
+    
+    return x
 
 def create_sample_data_n(length=1000, scale=1):
     x = create_time_data_n(length, scale)
@@ -44,6 +104,20 @@ def create_sample_data_n(length=1000, scale=1):
     A     = create_trend_data(x_data, regression="n")
     b     = y_data
     alpha = np.dot(np.linalg.inv(np.dot(A.T, A)), np.dot(A.T, b))
+    
+    return x[-1], alpha[0, 0]
+
+def create_sample_data_n_lag(lags=2, length=1000, coef=np.array([0.5, 0.5]), scale=1):
+    x = create_time_data_n_lag(lags, length, coef, scale)
+    
+    train_data = np.diff(x.reshape([length, 1]), axis=0)
+    x_data     = np.array([train_data[t-lags : t][::-1].ravel() for t in range(lags, length-1)])
+    x_data     = np.hstack([x[lags:-1].reshape([x_data.shape[0], 1]), x_data])
+    y_data     = x[lags+1:].reshape([x_data.shape[0], 1])
+    
+    A     = create_trend_data(x_data, regression="n")
+    b     = y_data
+    alpha = np.dot(np.linalg.inv( np.dot(A.T, A)), np.dot(A.T, b))
     
     return x[-1], alpha[0, 0]
 
@@ -60,6 +134,20 @@ def create_sample_data_c(length=1000, scale=1, const=10):
     
     return x[-1], alpha[0, 0]
 
+def create_sample_data_c_lag(lags=2, length=1000, coef=np.array([0.5, 0.5]), scale=1, const=10):
+    x = create_time_data_c_lag(lags, length, coef, scale, const)
+    
+    train_data = np.diff(x.reshape([length, 1]), axis=0)
+    x_data     = np.array([train_data[t-lags : t][::-1].ravel() for t in range(lags, length-1)])
+    x_data     = np.hstack([x[lags:-1].reshape([x_data.shape[0], 1]), x_data])
+    y_data     = x[lags+1:].reshape([x_data.shape[0], 1])
+    
+    A     = create_trend_data(x_data, regression="c")
+    b     = y_data
+    alpha = np.dot(np.linalg.inv( np.dot(A.T, A)), np.dot(A.T, b))
+    
+    return x[-1], alpha[0, 0]
+
 def create_sample_data_ct(length=1000, scale=1, const=10, δ1=2):
     x = create_time_data_ct(length, scale, const, δ1)
     
@@ -70,6 +158,20 @@ def create_sample_data_ct(length=1000, scale=1, const=10, δ1=2):
     A     = create_trend_data(x_data, regression="ct")
     b     = y_data
     alpha = np.dot(np.linalg.inv(np.dot(A.T, A)), np.dot(A.T, b))
+    
+    return x[-1], alpha[0, 0]
+
+def create_sample_data_ct_lag(lags=2, length=1000, coef=np.array([0.5, 0.5]), scale=1, const=10, δ1=2):
+    x = create_time_data_ct_lag(lags, length, coef, scale, const, δ1)
+    
+    train_data = np.diff(x.reshape([length, 1]), axis=0)
+    x_data     = np.array([train_data[t-lags : t][::-1].ravel() for t in range(lags, length-1)])
+    x_data     = np.hstack([x[lags:-1].reshape([x_data.shape[0], 1]), x_data])
+    y_data     = x[lags+1:].reshape([x_data.shape[0], 1])
+    
+    A     = create_trend_data(x_data, regression="ct")
+    b     = y_data
+    alpha = np.dot(np.linalg.inv( np.dot(A.T, A)), np.dot(A.T, b))
     
     return x[-1], alpha[0, 0]
 
@@ -86,6 +188,20 @@ def create_sample_data_ctt(length=1000, scale=1, const=10, δ1=2, δ2=1.1):
     
     return x[-1], alpha[0, 0]
 
+def create_sample_data_ctt_lag(lags=2, length=1000, coef=np.array([0.5, 0.5]), scale=1, const=10, δ1=2, δ2=1.1):
+    x = create_time_data_ctt_lag(lags, length, coef, scale, const, δ1, δ2)
+    
+    train_data = np.diff(x.reshape([length, 1]), axis=0)
+    x_data     = np.array([train_data[t-lags : t][::-1].ravel() for t in range(lags, length-1)])
+    x_data     = np.hstack([x[lags:-1].reshape([x_data.shape[0], 1]), x_data])
+    y_data     = x[lags+1:].reshape([x_data.shape[0], 1])
+    
+    A     = create_trend_data(x_data, regression="ctt")
+    b     = y_data
+    alpha = np.dot(np.linalg.inv( np.dot(A.T, A)), np.dot(A.T, b))
+    
+    return x[-1], alpha[0, 0]
+
 def create_time_data(input_list):
     regression, length, scale, const, δ1, δ2 = input_list
     
@@ -97,6 +213,22 @@ def create_time_data(input_list):
         x, alpha = create_sample_data_ct( length, scale, const, δ1)
     elif regression == "ctt":
         x, alpha = create_sample_data_ctt(length, scale, const, δ1, δ2)
+    else:
+        raise
+    
+    return x, alpha
+
+def create_time_data_lag(input_list):
+    regression, lags, length, coef, scale, const, δ1, δ2 = input_list
+    
+    if   regression == "n":
+        x, alpha = create_sample_data_n_lag(  lags, length, coef, scale)
+    elif regression == "c":
+        x, alpha = create_sample_data_c_lag(  lags, length, coef, scale, const)
+    elif regression == "ct":
+        x, alpha = create_sample_data_ct_lag( lags, length, coef, scale, const, δ1)
+    elif regression == "ctt":
+        x, alpha = create_sample_data_ctt_lag(lags, length, coef, scale, const, δ1, δ2)
     else:
         raise
     
--- time_series_model.py
+++ time_series_model.py
@@ -2,7 +2,6 @@ import numpy as np
 import scipy as sp
 import pandas as pd
 from scipy import stats
-from random import Random
 
 def modified_cholesky(x):
     if type(x) is list:
@@ -814,6 +813,11 @@ class Dickey_Fuller_Test:
         return y_pred
     
     def dfRuller(self, qlist=[0.2, 0.8, 2.5, 5, 10]):
+        if not self.learn_flg:
+            print(f"learn_flg = {self.learn_flg}")
+            print("エラー：：学習が完了していません。")
+            raise
+        
         save_ρ = pd.read_csv("./csv_data/DF_distribution.csv.zst", header=0, compression="zstd").to_dict(orient="list")        
         
         if   self.regression == "n":  # 定数項なし&トレンドなし
@@ -1090,7 +1094,7 @@ class Augmented_Dickey_Fuller_Test:
         if isVisible == True:
             print(f"AR model | {ic}", flush=True)
 
-        nobs = len(self.train_data)
+        nobs = len(self.test_data)
         if nobs <= maxlag:
             maxlag = nobs - 1
 
@@ -1127,166 +1131,42 @@ class Augmented_Dickey_Fuller_Test:
 
         return self.lags
     
-    def adfRuller(self):
-        tau_star_nc  = [-1.04, -1.53, -2.68, -3.09, -3.07, -3.77]
-        tau_min_nc   = [-19.04, -19.62, -21.21, -23.25, -21.63, -25.74]
-        tau_max_nc   = [np.inf, 1.51, 0.86, 0.88, 1.05, 1.24]
-        tau_star_c   = [-1.61, -2.62, -3.13, -3.47, -3.78, -3.93]
-        tau_min_c    = [-18.83, -18.86, -23.48, -28.07, -25.96, -23.27]
-        tau_max_c    = [2.74, 0.92, 0.55, 0.61, 0.79, 1]
-        tau_star_ct  = [-2.89, -3.19, -3.50, -3.65, -3.80, -4.36]
-        tau_min_ct   = [-16.18, -21.15, -25.37, -26.63, -26.53, -26.18]
-        tau_max_ct   = [0.7, 0.63, 0.71, 0.93, 1.19, 1.42]
-        tau_star_ctt = [-3.21, -3.51, -3.81, -3.83, -4.12, -4.63]
-        tau_min_ctt  = [-17.17, -21.1, -24.33, -24.03, -24.33, -28.22]
-        tau_max_ctt  = [0.54, 0.79, 1.08, 1.43, 3.49, 1.92]
-
-        _tau_maxs = {
-            "n":   tau_max_nc,
-            "c":   tau_max_c,
-            "ct":  tau_max_ct,
-            "ctt": tau_max_ctt,
-        }
-        _tau_mins = {
-            "n":   tau_min_nc,
-            "c":   tau_min_c,
-            "ct":  tau_min_ct,
-            "ctt": tau_min_ctt,
-        }
-        _tau_stars = {
-            "n":   tau_star_nc,
-            "c":   tau_star_c,
-            "ct":  tau_star_ct,
-            "ctt": tau_star_ctt,
-        }
+    def adfRuller(self, qlist=[0.2, 0.8, 2.5, 5, 10]):
+        if not self.learn_flg:
+            print(f"learn_flg = {self.learn_flg}")
+            print("エラー：：学習が完了していません。")
+            raise
         
+        save_ρ = pd.read_csv("./csv_data/ADF_distribution.csv.zst", header=0, compression="zstd").to_dict(orient="list")        
         
-        small_scaling = np.array([1, 1, 1e-2])
-        tau_nc_smallp = [
-            [0.6344, 1.2378, 3.2496],
-            [1.9129, 1.3857, 3.5322],
-            [2.7648, 1.4502, 3.4186],
-            [3.4336, 1.4835, 3.19],
-            [4.0999, 1.5533, 3.59],
-            [4.5388, 1.5344, 2.9807]]
-        tau_nc_smallp = np.asarray(tau_nc_smallp)*small_scaling
-
-        tau_c_smallp = [
-            [2.1659, 1.4412, 3.8269],
-            [2.92, 1.5012, 3.9796],
-            [3.4699, 1.4856, 3.164],
-            [3.9673, 1.4777, 2.6315],
-            [4.5509, 1.5338, 2.9545],
-            [5.1399, 1.6036, 3.4445]]
-        tau_c_smallp = np.asarray(tau_c_smallp)*small_scaling
-
-        tau_ct_smallp = [
-            [3.2512, 1.6047, 4.9588],
-            [3.6646, 1.5419, 3.6448],
-            [4.0983, 1.5173, 2.9898],
-            [4.5844, 1.5338, 2.8796],
-            [5.0722, 1.5634, 2.9472],
-            [5.53, 1.5914, 3.0392]]
-        tau_ct_smallp = np.asarray(tau_ct_smallp)*small_scaling
-
-        tau_ctt_smallp = [
-            [4.0003, 1.658, 4.8288],
-            [4.3534, 1.6016, 3.7947],
-            [4.7343, 1.5768, 3.2396],
-            [5.214, 1.6077, 3.3449],
-            [5.6481, 1.6274, 3.3455],
-            [5.9296, 1.5929, 2.8223]]
-        tau_ctt_smallp = np.asarray(tau_ctt_smallp)*small_scaling
-
-        _tau_smallps = {
-            "n":   tau_nc_smallp,
-            "c":   tau_c_smallp,
-            "ct":  tau_ct_smallp,
-            "ctt": tau_ctt_smallp,
-        }
-
-
-        large_scaling = np.array([1, 1e-1, 1e-1, 1e-2])
-        tau_nc_largep = [
-            [0.4797, 9.3557, -0.6999, 3.3066],
-            [1.5578, 8.558, -2.083, -3.3549],
-            [2.2268, 6.8093, -3.2362, -5.4448],
-            [2.7654, 6.4502, -3.0811, -4.4946],
-            [3.2684, 6.8051, -2.6778, -3.4972],
-            [3.7268, 7.167, -2.3648, -2.8288]]
-        tau_nc_largep = np.asarray(tau_nc_largep)*large_scaling
-
-        tau_c_largep = [
-            [1.7339, 9.3202, -1.2745, -1.0368],
-            [2.1945, 6.4695, -2.9198, -4.2377],
-            [2.5893, 4.5168, -3.6529, -5.0074],
-            [3.0387, 4.5452, -3.3666, -4.1921],
-            [3.5049, 5.2098, -2.9158, -3.3468],
-            [3.9489, 5.8933, -2.5359, -2.721]]
-        tau_c_largep = np.asarray(tau_c_largep)*large_scaling
-
-        tau_ct_largep = [
-            [2.5261, 6.1654, -3.7956, -6.0285],
-            [2.85, 5.272, -3.6622, -5.1695],
-            [3.221, 5.255, -3.2685, -4.1501],
-            [3.652, 5.9758, -2.7483, -3.2081],
-            [4.0712, 6.6428, -2.3464, -2.546],
-            [4.4735, 7.1757, -2.0681, -2.1196]]
-        tau_ct_largep = np.asarray(tau_ct_largep)*large_scaling
-
-        tau_ctt_largep = [
-            [3.0778, 4.9529, -4.1477, -5.9359],
-            [3.4713, 5.967, -3.2507, -4.2286],
-            [3.8637, 6.7852, -2.6286, -3.1381],
-            [4.2736, 7.6199, -2.1534, -2.4026],
-            [4.6679, 8.2618, -1.822, -1.9147],
-            [5.0009, 8.3735, -1.6994, -1.6928]]
-        tau_ctt_largep = np.asarray(tau_ctt_largep)*large_scaling
-
-        _tau_largeps = {
-            "n":   tau_nc_largep,
-            "c":   tau_c_largep,
-            "ct":  tau_ct_largep,
-            "ctt": tau_ctt_largep,
-        }
-        
-        
-        maxstat  = _tau_maxs[self.regression]
-        minstat  = _tau_mins[self.regression]
-        starstat = _tau_stars[self.regression]
-        
-        
-        x_d_all  = self.train_data
         if   self.regression == "n":  # 定数項なし&トレンドなし
-            esti_coef = np.hstack([self.alpha]).ravel()
-            
+            esti_coef = save_ρ["n"]
         elif self.regression == "c":  # 定数項あり&トレンドなし
-            esti_coef = np.hstack([self.alpha.T, self.alpha0]).ravel()
-        
+            esti_coef = save_ρ["c"]
         elif self.regression == "ct": # 定数項あり&1次のトレンドあり
-            esti_coef = np.hstack([self.alpha.T, self.alpha0, self.trend_1st]).ravel()
-            
+            esti_coef = save_ρ["ct"]
         elif self.regression == "ctt":# 定数項あり&1次のトレンドあり&2次のトレンドあり
-            esti_coef = np.hstack([self.alpha.T, self.alpha0, self.trend_1st, self.trend_2nd]).ravel()
+            esti_coef = save_ρ["ctt"]
         
+        min_val = np.min(esti_coef)
+        max_val = np.max(esti_coef)
+        q_val   = np.percentile(esti_coef, q=qlist)
         
-        self.tvalue = (esti_coef - 1) / np.sqrt(np.diag(self.sigma * np.linalg.inv(np.dot(x_d_all.T, x_d_all))))
-        tvalue      = self.tvalue[0]
-        if tvalue > maxstat[0]:
-            self.pvalue = 1.0
-            return self.pvalue
+        dict_q = {}
+        for q, val in zip(qlist, q_val):
+            dict_q[str(q) + "%"] = val
         
-        elif tvalue < minstat[0]:
+        self.ρvalue = self.alpha[0, 0]
+        if   self.ρvalue < min_val:
             self.pvalue = 0.0
-            return self.pvalue
-        
-        if tvalue <= starstat[0]:
-            tau_coef    = _tau_smallps[self.regression][0]
-            self.pvalue = sp.stats.norm.cdf(np.polyval(tau_coef[::-1], tvalue))
+        elif self.ρvalue > max_val:
+            self.pvalue = 1.0
         else:
-            # Note: above is only for z stats
-            tau_coef    = _tau_largeps[self.regression][0]
-            self.pvalue = sp.stats.norm.cdf(np.polyval(tau_coef[::-1], tvalue))
+            np_coef = np.sort(esti_coef)
+            num = len(esti_coef)
+            index = np.abs(np_coef - self.ρvalue).argsort()[0].tolist()
+            
+            self.pvalue = (index + 1) / num
         
-        return tvalue, self.pvalue
+        return self.ρvalue, self.pvalue, self.lags, self.data_num, dict_q
 
