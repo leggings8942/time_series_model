154c2c0780dd34b975c96809016c956744ea6d3f
h_nagino <h_nagino@ADN23078MacBook-Pro.local>
2024-11-15 17:31:49 +0900

    Ridge最適化及びLasso最適化の実装完了

--- time_series_model.py
+++ time_series_model.py
@@ -102,7 +102,7 @@ class Auto_Regressive:
         # OLS(Ordinary Learst Squares)推定量を計算する際に
         # 擬似逆行列(pinv関数)を使用している箇所が存在する
         # この処理は逆行列が存在しない場合(行列式が0の場合)に発火する
-        # しかし理論的には逆行列が存在しない時系列データの組み合わせは存在しない
+        # しかし理論的には逆行列が存在しない時系列データの組み合わせは極めて稀である(無視できる)
         # 入力された時系列データ自体にミスが存在する(0の定数列になっている等)可能性が高い
         # statsmodels.tsa.vector_ar.var_model.VARではこのような時系列データを入力として与えた場合には
         # エラーを出力するようになっている
@@ -354,7 +354,7 @@ class Vector_Auto_Regressive:
         # OLS(Ordinary Learst Squares)推定量を計算する際に
         # 擬似逆行列(pinv関数)を使用している箇所が存在する
         # この処理は逆行列が存在しない場合(行列式が0の場合)に発火する
-        # しかし理論的には逆行列が存在しない時系列データの組み合わせは存在しない
+        # しかし理論的には逆行列が存在しない時系列データの組み合わせは極めて稀である(無視できる)
         # 入力された時系列データ自体にミスが存在する(0の定数列になっている等)可能性が高い
         # statsmodels.tsa.vector_ar.var_model.VARではこのような時系列データを入力として与えた場合には
         # エラーを出力するようになっている
@@ -682,7 +682,448 @@ class Vector_Auto_Regressive:
             fevd[idx, :, :] = fevd[idx, :, :] / np.sum(fevd[idx, :, :], axis=1).reshape([self.train_data.shape[1], 1])"""
         
         return fevd
+
+
+class Sparse_Vector_Auto_Regressive:
+    def __init__(self, train_data, l1_norm:float=0.01, l2_norm:float=1.0, tol:float=1e-7, max_iterate:int=100000, learning_rate:float=0.001, random_state=None) -> None:
+        if type(train_data) is pd.core.frame.DataFrame:
+            train_data = train_data.to_numpy()
+        
+        if type(train_data) is list:
+            train_data = np.array(train_data)
+        
+        if train_data.ndim != 2:
+            print(f"train_data dims = {train_data.ndim}")
+            print("エラー：：次元数が一致しません。")
+            raise
+        
+        self.train_data          = train_data
+        self.lags                = 0
+        self.alpha               = np.zeros([1, 1])
+        self.alpha0              = np.zeros([1, 1])
+        self.sigma               = np.zeros([1, 1])
+        self.l1_norm             = l1_norm
+        self.l2_norm             = l2_norm
+        self.tol                 = tol
+        self.solver              = ""
+        self.data_num            = 0
+        self.max_iterate         = max_iterate
+        self.correct_alpha       = Update_Rafael(alpha=learning_rate)
+        self.correct_alpha0      = Update_Rafael(alpha=learning_rate)
+        self.unbiased_dispersion = 0
+        self.dispersion          = 0
+        self.ma_inf              = np.zeros([1, 1])
+        self.learn_flg           = False
+
+        self.random_state = random_state
+        if random_state != None:
+            self.random = np.random
+            self.random.seed(seed=self.random_state)
+        else:
+            self.random = np.random
+            
+    def copy(self):
+        buf = []
+        buf = buf + [self.train_data.copy()]
+        buf = buf + [self.lags]
+        buf = buf + [self.alpha.copy()]
+        buf = buf + [self.alpha0.copy()]
+        buf = buf + [self.sigma.copy()]
+        buf = buf + [self.l1_norm]
+        buf = buf + [self.l2_norm]
+        buf = buf + [self.tol]
+        buf = buf + [self.solver]
+        buf = buf + [self.data_num]
+        buf = buf + [self.max_iterate]
+        buf = buf + [self.correct_alpha]
+        buf = buf + [self.correct_alpha0]
+        buf = buf + [self.unbiased_dispersion]
+        buf = buf + [self.dispersion]
+        buf = buf + [self.ma_inf.copy()]
+        buf = buf + [self.learn_flg]
+        buf = buf + [self.random_state]
+        buf = buf + [self.random]
+        
+        return buf
     
+    def restore(self, buf):
+        self.train_data          = buf[0]
+        self.lags                = buf[1]
+        self.alpha               = buf[2]
+        self.alpha0              = buf[3]
+        self.sigma               = buf[4]
+        self.l1_norm             = buf[5]
+        self.l2_norm             = buf[6]
+        self.tol                 = buf[7]
+        self.solver              = buf[8]
+        self.data_num            = buf[9]
+        self.max_iterate         = buf[10]
+        self.correct_alpha       = buf[11]
+        self.correct_alpha0      = buf[12]
+        self.unbiased_dispersion = buf[13]
+        self.dispersion          = buf[14]
+        self.ma_inf              = buf[15]
+        self.learn_flg           = buf[16]
+        self.random_state        = buf[17]
+        self.random              = buf[18]
+        
+        return True
+
+    def fit(self, lags:int=1, offset:int=0, solver:str='normal equations') -> bool:
+        # caution!!!
+        # OLS(Ordinary Learst Squares)推定量を計算する際に
+        # 擬似逆行列(pinv関数)を使用している箇所が存在する
+        # この処理は逆行列が存在しない場合(行列式が0の場合)に発火する
+        # しかし理論的には逆行列が存在しない時系列データの組み合わせは極めて稀である(無視できる)
+        # 入力された時系列データ自体にミスが存在する(0の定数列になっている等)可能性が高い
+        # statsmodels.tsa.vector_ar.var_model.VARではこのような時系列データを入力として与えた場合には
+        # エラーを出力するようになっている
+        # 本ライブラリにおいてエラーの出力を行わないのは、近似的にでも処理結果が欲しいためである
+        
+        if len(self.train_data) <= offset:
+            # データ数に対して、オフセットが大き過ぎる
+            self.learn_flg = False
+            return self.learn_flg
+        
+        tmp_train_data = self.train_data[offset:]
+        nobs           = len(tmp_train_data)
+        
+        if nobs <= lags:
+            # 学習対象データ数に対して、ラグが大き過ぎる
+            self.learn_flg = False
+            return self.learn_flg
+        
+        x_data         = np.array([tmp_train_data[t-lags : t][::-1].ravel() for t in range(lags, nobs)])
+        y_data         = tmp_train_data[lags:]
+        
+        self.lags = lags
+        num, s    = x_data.shape
+        if solver == "normal equations":
+            # ラッソ最適化(L1正則化)とリッジ最適化(L2正則化)を行なっている
+            # 注意点として、切片に対してはラッソ最適化を行わないことが挙げられる
+            # リッジ最適化は一般に係数を0にするためではなく、最適化対象のパラメータ全体を小さく保つために利用される
+            # 一方で、ラッソ最適化は係数を0にするために利用される手法である
+            # そのため、一般にはラッソ最適化を切片に対しては適用しない習慣がある
+            # このライブラリもこの習慣に従うことにする
+            
+            #正規方程式
+            A = np.hstack([x_data, np.ones([num, 1])])
+            b = y_data
+            try:
+                tmp_L  = np.dot(A.T, A) + self.l2_norm * np.identity(s + 1)
+                tmp_R1 = np.dot(A.T, b)
+                tmp_R2 = self.l1_norm * np.sign(tmp_R1)
+                tmp_R2[-1, :] = 0   # 切片に対するL1正則効果を無効にする
+                x      = np.dot(np.linalg.inv(tmp_L),  tmp_R1 - tmp_R2)
+                x[np.abs(x) < self.l1_norm] = 0
+            except np.linalg.LinAlgError as e:
+                tmp_L  = np.dot(A.T, A) + self.l2_norm * np.identity(s + 1)
+                tmp_R1 = np.dot(A.T, b)
+                tmp_R2 = self.l1_norm * np.sign(tmp_R1)
+                tmp_R2[-1, :] = 0   # 切片に対するL1正則効果を無効にする
+                x      = np.dot(np.linalg.pinv(tmp_L), tmp_R1 - tmp_R2)
+                x[np.abs(x) < self.l1_norm] = 0
+                
+            self.alpha, self.alpha0 = x[0:s, :], x[s, :]
+            self.alpha0 = self.alpha0.reshape([1, x.shape[1]])
+        else:
+            raise
+        
+        # 不偏共分散行列を計算するためには、本来以下のような母数を採用する必要がある
+        # 母数 = 学習データ数 - 最適化対象変数の数
+        # しかし、このモデルでは”学習データ数 << 最適化対象変数の数”という状況下で利用されることを想定している
+        # この状況下では、母数が負の値になってしまうため採用することができない
+        # そのため、苦肉の策として 母数 = 学習データ数 - 1 を採用することにした。根拠は弱い。
+        # 今後、このような状況での最適な不偏共分散行列の求め方が判明したならば積極的に変更を加えることとする
+        denominator    = num - 1
+        
+        self.learn_flg = True
+        y_pred         = self.predict(x_data)
+        diff           = y_pred - y_data
+        self.sigma     = np.dot(diff.T, diff) / denominator
+        self.solver    = solver
+        self.data_num  = num
+        self.unbiased_dispersion = denominator
+        self.dispersion          = y_data.shape[0]
+
+        return self.learn_flg
+
+    def predict(self, test_data) -> np.ndarray:
+        if type(test_data) is pd.core.frame.DataFrame:
+            test_data = test_data.to_numpy()
+        
+        if type(test_data) is list:
+            test_data = np.array(test_data)
+        
+        if test_data.ndim != 2:
+            print(f"test_data dims = {test_data.ndim}")
+            print("エラー：：次元数が一致しません。")
+            raise
+        
+        if not self.learn_flg:
+            print(f"learn_flg = {self.learn_flg}")
+            print("エラー：：学習が完了していません。")
+            raise
+        
+        y_pred = np.dot(test_data, self.alpha) + self.alpha0
+        
+        return y_pred
+    
+    def get_RSS(self) -> np.ndarray:
+        nobs   = len(self.train_data)
+        x_data = np.array([self.train_data[t-self.lags : t][::-1].ravel() for t in range(self.lags, nobs)])
+        y_data = self.train_data[self.lags:]
+        
+        y_pred = self.predict(x_data)
+
+        rss = np.square(y_data - y_pred)
+        rss = np.sum(rss, axis=0)
+        return rss
+    
+    def log_likelihood(self) -> np.float64:
+        # なぜか、対数尤度の計算に特殊な計算方法が採用されている
+        # statsmodels.tsa.vector_ar.var_model を参照のこと
+        # var_loglike関数内にて当該の記述を発見
+        # どうやらこれで対数尤度を計算できるらしい
+        # math:: -\left(\frac{T}{2}\right) \left(\ln\left|\Omega\right| - K\ln\left(2\pi\right) - K\right)
+        # この式が元になっているらしい
+        # さっぱり理解できないため、通常通りに計算することにする
+        
+        if not self.learn_flg:
+            print(f"learn_flg = {self.learn_flg}")
+            print("エラー：：学習が完了していません。")
+            raise
+        
+        nobs   = len(self.train_data)
+        x_data = np.array([self.train_data[t-self.lags : t][::-1].ravel() for t in range(self.lags, nobs)])
+        y_data = self.train_data[self.lags:]
+
+        num, _ = y_data.shape
+        y_pred = self.predict(x_data)
+
+        # 不偏推定共分散量を通常の推定共分散量に直す
+        tmp_sigma      = self.sigma * self.unbiased_dispersion / self.dispersion
+        
+        prob           = [multivariate_normal_distrubution(y_data[idx, :], y_pred[idx, :], tmp_sigma) for idx in range(0, num)]
+        prob           = np.array(prob).reshape([num, 1])
+        log_likelihood = np.sum(np.log(prob + 1e-32))
+
+        return log_likelihood
+    
+    def model_reliability(self, ic="aic") -> np.float64:
+        # statsmodels.tsa.vector_ar.var_model.VARResults を参照のこと
+        # info_criteria関数内にて当該の記述を発見
+        # 赤池情報基準やベイズ情報基準をはじめとした情報基準が特殊な形に変形されている
+        # これは、サンプル数を考慮した改良版らしい
+        # これを採用することとする
+        
+        if not self.learn_flg:
+            print(f"learn_flg = {self.learn_flg}")
+            print("エラー：：学習が完了していません。")
+            raise
+        
+        num = self.data_num
+        k   = self.alpha.size + self.alpha0.size
+        #log_likelihood = self.log_likelihood()
+        
+        # caution!!!
+        # 本ライブラリでは、データ数に対して最尤推定対象が多い場合にもできる限り処理を続けるように調整してある
+        # しかし、この場合に分散共分散行列の正定値性が保てなくなるという問題が発生する
+        # また、入力された時系列データ自体に誤りが存在する場合にも正定値性が保てなくなる
+        # 正定値行列でない場合には対数尤度の計算ができなくなる
+        # この問題の対策のために対数尤度の近似値を求める処理に変更していることに注意
+        # 参考URL:
+        # https://seetheworld1992.hatenablog.com/entry/2017/03/22/194932
+        
+        # 不偏推定共分散量を通常の推定共分散量に直す
+        tmp_sigma = self.sigma * self.unbiased_dispersion / self.dispersion
+        det_sigma = np.linalg.det(tmp_sigma)
+        det_sigma = det_sigma if det_sigma != 0 else 1e-16
+
+        inf = 0
+        if ic == "aic":
+            #inf = -2 * log_likelihood + 2 * k
+            inf = np.log(np.abs(det_sigma)) + 2 * k / num
+        elif ic == "bic":
+            #inf = -2 * log_likelihood + k * np.log(num)
+            inf = np.log(np.abs(det_sigma)) + k * np.log(num) / num
+        elif ic == "hqic":
+            #inf = -2 * log_likelihood + 2 * k * np.log(np.log(num))
+            inf = np.log(np.abs(det_sigma)) + 2 * k * np.log(np.log(num)) / num
+        else:
+            raise
+
+        return inf
+
+    def select_order(self, maxlag=15, ic="aic", solver="normal equations", isVisible=False) -> int:
+        if isVisible == True:
+            print(f"AR model | {ic}", flush=True)
+        
+        nobs = len(self.train_data)
+        if nobs <= maxlag:
+            maxlag = nobs - 1
+
+        model_param = []
+        for lag in range(1, maxlag + 1):
+            flg = self.fit(lags=lag, offset=maxlag - lag, solver=solver)
+            
+            if flg:
+                rel = self.model_reliability(ic=ic)
+                model_param.append([rel, lag])
+            else:
+                rel = np.finfo(np.float64).max
+                model_param.append([rel, lag])
+
+            if isVisible == True:
+                print(f"AR({lag}) | {rel}", flush=True)
+        
+        res_rel, res_lag = np.finfo(np.float64).max, 0
+        for elem in model_param:
+            tmp_rel, tmp_lag = elem
+            if res_rel > tmp_rel:
+                res_rel = tmp_rel
+                res_lag = tmp_lag
+        
+        res_lag = res_lag if res_lag != 0 else 1
+        self.fit(lags=res_lag, offset=0, solver=solver)
+        if not self.learn_flg:
+            print(f"learn_flg = {self.learn_flg}")
+            print("エラー：：学習が完了しませんでした。")
+            raise
+        
+        if isVisible == True:
+            print(f"selected orders | {res_lag}", flush=True)
+
+        return self.lags
+    
+    def stat_inf(self) -> dict:
+        info = {}
+        if self.lags * self.alpha.shape[1] != self.alpha.shape[0]:
+            print("データの次元が一致しません")
+            print("lags = ", self.lags)
+            print("alpha.shape = ", self.alpha.shape)
+            print("時系列データ数 = ", self.alpha.shape[1])
+            raise
+
+        tmp_alpha = []
+        for lag in range(0, self.lags):
+            tmp_alpha.append(self.alpha[self.alpha.shape[1] * lag:self.alpha.shape[1] * (lag + 1), :].tolist())
+
+        tmp_alpha = np.array(tmp_alpha)
+        info["mean"] = np.dot(np.linalg.inv(np.identity(self.alpha.shape[1]) - np.sum(tmp_alpha, axis=0)), self.alpha0.T)
+
+        return info
+    
+    def test_causality(self, causing=0, caused=1):
+        backup = self.copy()
+        tmp_train_data = backup[0]
+        tmp_lags       = backup[1]
+        tmp_alpha      = backup[2]
+        tmp_solver     = backup[8]
+        tmp_data_num   = backup[9]
+
+        self.fit(lags=tmp_lags, solver=tmp_solver)
+        rss1 = self.get_RSS()[caused]
+
+        caused = caused - 1 if causing < caused else caused
+        self.train_data = np.delete(tmp_train_data, causing, axis=1)
+        self.fit(lags=tmp_lags, solver=tmp_solver)
+        rss0 = self.get_RSS()[caused]
+
+        num    = tmp_train_data.shape[1]
+        Fvalue = (rss0 - rss1)/num / (rss1 / (tmp_data_num - tmp_alpha.shape[0] - 1))
+        pvalue = stats.chi2.sf(x=Fvalue*num, df=num)
+
+        self.restore(backup)
+
+        return Fvalue*num, pvalue
+    
+    def ma_replace(self, max=10):
+        ma_inf = np.zeros([max + 1, self.train_data.shape[1], self.train_data.shape[1]])
+        ma_inf[0, :, :] = np.identity(self.train_data.shape[1])
+        
+        x_data = ma_inf[0, :, :]
+        for _ in range(1, self.lags):
+            x_data = np.vstack([x_data, np.zeros([self.train_data.shape[1], self.train_data.shape[1]])])
+
+        for idx in range(1, max + 1):
+            ma_inf[idx, :, :] = np.dot(self.alpha.T, x_data)
+            x_data = np.vstack([ma_inf[idx, :, :], x_data[:-self.train_data.shape[1], :]])
+        
+        self.ma_inf = ma_inf
+        return self.ma_inf
+
+    def irf(self, period=30, orth=False, isStdDevShock=True):
+        if not self.learn_flg:
+            print(f"learn_flg = {self.learn_flg}")
+            print("エラー：：学習が完了していません。")
+            raise
+
+        if orth == True:
+            A, D = modified_cholesky(self.sigma)
+
+            irf = np.zeros([period + 1, self.train_data.shape[1], self.train_data.shape[1]])
+            if isStdDevShock:
+                irf[0, :, :] = np.dot(A, np.sqrt(D))
+            else:
+                irf[0, :, :] = np.dot(A, np.identity(self.train_data.shape[1]))
+
+            x_data = irf[0, :, :]
+            for _ in range(1, self.lags):
+                x_data = np.vstack([x_data, np.zeros([self.train_data.shape[1], self.train_data.shape[1]])])
+            
+            for idx in range(1, period + 1):
+                #tmp = self.alpha.reshape(self.lags, self.train_data.shape[1], self.train_data.shape[1])
+                #tmp = tmp.swapaxes(1,2).reshape(self.lags * self.train_data.shape[1], self.train_data.shape[1])
+                #irf[idx, :, :] = np.dot(x_data, tmp)
+                irf[idx, :, :] = np.dot(self.alpha.T, x_data)
+                x_data = np.vstack([irf[idx, :, :], x_data[:-self.train_data.shape[1], :]])
+            """irf_data = self.irf(period, orth=False)
+            L = np.linalg.cholesky(self.sigma)
+            irf = np.array([np.dot(coefs, L) for coefs in irf_data])"""
+
+        else:
+            irf = self.ma_replace(period)
+
+        return irf
+    
+    def fevd(self, period=30):
+        if not self.learn_flg:
+            print(f"learn_flg = {self.learn_flg}")
+            print("エラー：：学習が完了していません。")
+            raise
+        
+        # 不偏推定共分散量を通常の推定共分散量に直す
+        tmp_sigma = self.sigma * self.unbiased_dispersion / self.dispersion
+        A, D = modified_cholesky(tmp_sigma)
+        
+        fevd = np.zeros([period + 1, self.train_data.shape[1], self.train_data.shape[1]])
+        fevd[0, :, :] = A
+        
+        x_data = fevd[0, :, :]
+        for _ in range(1, self.lags):
+            x_data = np.vstack([x_data, np.zeros([self.train_data.shape[1], self.train_data.shape[1]])])
+        
+        fevd[0, :, :] = fevd[0, :, :] ** 2
+        for idx in range(1, period + 1):
+            fevd[idx, :, :] = np.dot(self.alpha.T, x_data)
+            x_data = np.vstack([fevd[idx, :, :], x_data[:-self.train_data.shape[1], :]])
+            
+            fevd[idx, :, :] = fevd[idx, :, :] ** 2
+        
+        fevd = fevd.cumsum(axis=0)
+        for idx in range(0, period + 1):
+            fevd[idx, :, :] = np.dot(fevd[idx, :, :], D)
+        
+        for idx in range(0, period + 1):
+            fevd[idx, :, :] = fevd[idx, :, :] / np.sum(fevd[idx, :, :], axis=1).reshape([self.train_data.shape[1], 1])
+        """fevd = self.irf(period=period, orth=True)
+        fevd = (fevd ** 2).cumsum(axis=0)
+        for idx in range(0, period + 1):
+            fevd[idx, :, :] = fevd[idx, :, :] / np.sum(fevd[idx, :, :], axis=1).reshape([self.train_data.shape[1], 1])"""
+        
+        return fevd
+
 
 class Dickey_Fuller_Test:
     def __init__(self, test_data, regression="c") -> None:
@@ -718,7 +1159,7 @@ class Dickey_Fuller_Test:
         # OLS(Ordinary Learst Squares)推定量を計算する際に
         # 擬似逆行列(pinv関数)を使用している箇所が存在する
         # この処理は逆行列が存在しない場合(行列式が0の場合)に発火する
-        # しかし理論的には逆行列が存在しない時系列データの組み合わせは存在しない
+        # しかし理論的には逆行列が存在しない時系列データの組み合わせは極めて稀である(無視できる)
         # 入力された時系列データ自体にミスが存在する(0の定数列になっている等)可能性が高い
         # statsmodels.tsa.vector_ar.var_model.VARではこのような時系列データを入力として与えた場合には
         # エラーを出力するようになっている
@@ -927,7 +1368,7 @@ class Augmented_Dickey_Fuller_Test:
         # OLS(Ordinary Learst Squares)推定量を計算する際に
         # 擬似逆行列(pinv関数)を使用している箇所が存在する
         # この処理は逆行列が存在しない場合(行列式が0の場合)に発火する
-        # しかし理論的には逆行列が存在しない時系列データの組み合わせは存在しない
+        # しかし理論的には逆行列が存在しない時系列データの組み合わせは極めて稀である(無視できる)
         # 入力された時系列データ自体にミスが存在する(0の定数列になっている等)可能性が高い
         # statsmodels.tsa.vector_ar.var_model.VARではこのような時系列データを入力として与えた場合には
         # エラーを出力するようになっている
